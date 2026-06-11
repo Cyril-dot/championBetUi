d5b5975b96352dc4e500a36e2091887128ed8089
Ottih Chuks <chukwukaottih@gmail.com>
2026-06-11 17:43:53 +0000

    Update DepositPage.tsx

--- src/pages/DepositPage.tsx
+++ src/pages/DepositPage.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
 const API_BASE = "https://futballbackend-production-13f1.up.railway.app";
-const MIN_DEPOSIT_GHS = 100;
+const MIN_DEPOSIT_GHS =300;
 
 /* ─── Bank Transfer Details (Nigeria) ───────────────────────────────────────── */
 const BANK_NAME        = "UBA";
